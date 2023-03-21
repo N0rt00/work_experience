f709b6246e71dd581bee105c3f7a1426a48d2546
N0rt00 <sam.thornton07@icloud.com>
2023-03-21 16:26:16 +1100

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+import 'page2.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopping Mate',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Shopping Mate'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var colour = Colors.red;
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  _login() {
+    setState(() {
+      if (usernameController.text == 'user' &&
+          passwordController.text == 'password') {
+        print('right');
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const SecondPage()));
+      } else {
+        print('wrong');
+      }
+    });
+  }
+
+  _forgotPasswd() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 40, bottom: 40),
+              child: SizedBox(
+                width: 170,
+                height: 170,
+                child: Image.asset('assets/pngegg.png'),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 40),
+              child: Text(
+                'SHOPPING\nMATE',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 40,
+                ),
+              ),
+            ),
+            FractionallySizedBox(
+              widthFactor: 0.8,
+              child: TextField(
+                controller: usernameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter Your Username',
+                  labelText: 'Username',
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            FractionallySizedBox(
+              widthFactor: 0.8,
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter Your Password',
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: _forgotPasswd,
+              child: const Text(
+                'Forgot Password',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            SizedBox(
+              width: 300,
+              child: ElevatedButton(
+                onPressed: _login,
+                child: Text(
+                  'Login',
+                  style: TextStyle(fontSize: 20, color: colour),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/page2.dart
@@ -0,0 +1,125 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatefulWidget {
+  const SecondPage({super.key});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  List<String> items = [];
+  TextEditingController itemController = TextEditingController();
+
+  _modifyList() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              height: 100,
+              color: const Color.fromARGB(255, 217, 217, 217),
+              child: const Text(
+                'SHOPPING\nMATE',
+                style: TextStyle(fontSize: 40),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: SizedBox(
+                width: double.infinity,
+                height: 150,
+                child: Column(
+                  children: [
+                    TextButton(
+                        onPressed: () {
+                          setState(() {
+                            items = [];
+                          });
+                        },
+                        child: const Text('Clear List')),
+                    TextButton(
+                        onPressed: () {
+                          setState(() {
+                            Navigator.pop(context);
+                          });
+                        },
+                        child: const Text('Log Out')),
+                    TextButton(
+                        onPressed: _modifyList,
+                        child: const Text('Modify List'))
+                  ],
+                ),
+              ),
+            ),
+            for (var i in items) Checkbox(foodName: i),
+            Row(
+              children: <Widget>[
+                TextButton(
+                    onPressed: () {
+                      setState(() {
+                        items.add(itemController.text);
+                      });
+                    },
+                    child: const Icon(Icons.add)),
+                SizedBox(
+                  width: 250,
+                  child: TextField(
+                    controller: itemController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter Item',
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Checkbox extends StatefulWidget {
+  final String foodName;
+  const Checkbox({super.key, required this.foodName});
+  final checkedValue = false;
+  @override
+  State<Checkbox> createState() => CheckboxState();
+}
+
+class CheckboxState extends State<Checkbox> {
+  bool? checkedValue = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return CheckboxListTile(
+      title: Text(widget.foodName),
+      value: checkedValue,
+      onChanged: (newValue) {
+        setState(() {
+          checkedValue = newValue;
+        });
+      },
+      controlAffinity: ListTileControlAffinity.leading,
+    );
+  }
+}
